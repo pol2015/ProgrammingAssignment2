a0864158d6326b4d10ae748d6601eb9512a36bb9
pol2015 <pol2015@users.noreply.github.com>
2015-04-26 11:36:58 -0300

    Update cachematrix.R
    Final working code

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,4 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+#similar function but uses solve and not mean
 
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
@@ -17,25 +14,9 @@ makeCacheMatrix <- function(x = matrix()) {
              getinv = getinv)
 }
 
-#makeVector <- function(x = numeric()) {
-#        m <- NULL
-#        set <- function(y) {
-#                x <<- y
-#                m <<- NULL
-#        }
-#        get <- function() x
-#        setmean <- function(mean) m <<- mean
-#        getmean <- function() m
-#        list(set = set, get = get,
-#             setmean = setmean,
-#             getmean = getmean)
-#}
-
-
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## Return a matrix that is the inverse of 'x' using the solve function
 
         m <- x$getinv()
         if(!is.null(m)) {
@@ -47,23 +28,7 @@ cacheSolve <- function(x, ...) {
         x$setinv(m)
         m
 
-
-
-
-
 }
 
 
 
-
-#cachemean <- function(x, ...) {
-#        m <- x$getmean()
-#        if(!is.null(m)) {
-#                message("getting cached data")
-#                return(m)
-#        }
-#        data <- x$get()
-#        m <- mean(data, ...)
-#        x$setmean(m)
-#        m
-#}
